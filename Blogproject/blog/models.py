403c66064af8b4ec11ffa96b2dd3f3f94e33b9af
6tus-nna <omejesixtusnnannamakuo@gmail.com>
2020-09-11 16:07:21 +0100

    admin site

--- Blogproject/blog/models.py
+++ Blogproject/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    body = models.TextField()
+
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    published = models.DateField(default=timezone.now())
+
+    class meta:
+        ordering = ('-published')
+        
+    def __str__(self):
+        return self.title
