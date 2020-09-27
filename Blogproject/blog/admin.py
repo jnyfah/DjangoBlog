303e33ee3edde37540b29aface0920d57d3e2fbc
6tus-nna <omejesixtusnnannamakuo@gmail.com>
2020-09-27 05:44:24 +0100

    new folders

--- Blogproject/blog/admin.py
+++ Blogproject/blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from.models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','author', 'published',)
+    list_filter = ('created', 'published', 'author',)
+    search_fields = ('title', 'body',)
+    raw_id_fields = ('author',)
+    date_hierarchy='published'
+    ordering = ('published',)
+
--- Blogproject/blog/models.py
+++ Blogproject/blog/models.py
@@ -4,14 +4,17 @@ from django.utils import timezone
 
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Model):
+    def get_queryset(self):
+        return super(PublishedManager, self) .get_queryset().filter(status='publish')
+
 class Post(models.Model):
     title = models.CharField(max_length=70)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body = models.TextField()
-
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
-    published = models.DateField(default=timezone.now())
+    published = models.DateField(default=timezone)
 
     class meta:
         ordering = ('-published')
